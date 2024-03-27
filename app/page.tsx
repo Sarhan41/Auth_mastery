e33c6d2e2a4509cddf2a5be5f7ef9447d48e465d
Sarhan41 <sp.webdev2024@gmail.com>
2024-03-28 04:30:46 +0530

    Update favicon and page layout***

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,11 @@ export default function Home() {
       className="flex h-full flex-col items-center justify-center 
     bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800"
     >
-      <div className="space-y-6">Hellow</div>
+      <div className="space-y-6">
+        <h1 className="text-6xl font-semibold text-white drop-shadow-md" >
+          Auth
+        </h1>
+      </div>
     </main>
   );
 }
