cce48fa79c807916ed7b1f51203ecab1b70cbd87
Sarhan41 <sp.webdev2024@gmail.com>
2024-04-01 14:35:54 +0530

    Fix session user role assignment in auth callbacks

--- auth.ts
+++ auth.ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 
+import { UserRole } from "@prisma/client";
 import { getUserById } from "@/data/user";
 import { db } from "./lib/db";
 import authConfig from "@/auth.config";
@@ -12,16 +13,29 @@ export const {
   signOut,
 } = NextAuth({
   callbacks: {
+    // async signIn({ user }) {
+    //   if (!user.id) {
+    //     return false;
+    //   }
+
+    //   const existingUser = await getUserById(user.id);
+
+    //   if (!existingUser || !existingUser.emailVerified) {
+    //     return false;
+    //   }
+
+    //   return true;
+    // },
     async session({ token, session }) {
       console.log({
         sessiontoken: token,
-      })
+      });
       if (token.sub && session.user) {
         session.user.id = token.sub;
       }
 
-      if(token.role && session.user) {
-        session.user.role = token.role;
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
       }
 
       return session;
@@ -29,7 +43,7 @@ export const {
     async jwt({ token }) {
       if (!token.sub) return token;
 
-      const existingUser = await getUserById(token.sub)
+      const existingUser = await getUserById(token.sub);
       if (!existingUser) return token;
 
       token.role = existingUser.role;
