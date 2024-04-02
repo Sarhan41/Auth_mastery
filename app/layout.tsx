f5a4b39301cedc0dfdf9d248dbcadab6ac268783
Sarhan41 <sp.webdev2024@gmail.com>
2024-04-02 11:42:18 +0530

    Add resend package and import sendVerificationEmail function

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { sendVerificationEmail } from "@/lib/mail";
 
 const inter = Inter({ subsets: ["latin"] });
 
---
+++ lib/mail.ts
@@ -0,0 +1,9 @@
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export const sendVerificationEmail = async (email: string, token: string) => {
+    const confirmLink = `${process.env.WEBPATH}/auth/new-verification?token=${token}`;
+console.log('confirmLink', confirmLink);
+
+};
